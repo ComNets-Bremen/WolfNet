8c0cd4aba3b724b03677ed8fa7f52b0d6b7145f3
Jens Dede <jd@comnets.uni-bremen.de>
2022-08-17 15:51:05 +0200

    Add new node, add new status LED and IRQ config

--- wolfnet/lib/config.py
+++ wolfnet/lib/config.py
@@ -15,7 +15,7 @@ import packets
 #
 
 """
-# ES32 TTGO v1.0 
+# ES32 TTGO v1.0
 device_config = {
     'miso':19,
     'mosi':27,
@@ -23,7 +23,7 @@ device_config = {
     'sck':5,
     'dio_0':26,
     'reset':14,
-    'led':2, 
+    'led':2,
 }
 
 # M5Stack ATOM Matrix
@@ -34,7 +34,7 @@ device_config = {
     'sck':33,
     'dio_0':25,
     'reset':21,
-    'led':12, 
+    'led':12,
 }
 """
 
@@ -46,7 +46,7 @@ device_config = {
     'sck':5,
     'dio_0':26,
     'reset':14,
-    'led':25, 
+    'led':25,
 }
 
 app_config = {
@@ -55,13 +55,13 @@ app_config = {
 }
 
 lora_parameters = {
-    'frequency': 868E6, 
-    'tx_power_level': 2, 
-    'signal_bandwidth': 125E3,    
-    'spreading_factor': 7, 
-    'coding_rate': 5, 
+    'frequency': 868E6,
+    'tx_power_level': 2,
+    'signal_bandwidth': 125E3,
+    'spreading_factor': 7,
+    'coding_rate': 5,
     'preamble_length': 8,
-    'implicit_header': False, 
+    'implicit_header': False,
     'sync_word': 0x12,  #0x12 = private, 0x34 = public
     'enable_CRC': False,
     'invert_IQ': False,
@@ -90,6 +90,7 @@ nodes_config = {
         "beacon_interval" : 240, # seconds
         "beacon_jitter" : 10,    # seconds, will vary the above value by +- 10 seconds
         "battery_type" : "max17043",
+        "gpio_button_irq" : 13, # IRQ on pin 13
     },
     "4291548233" : { # Flash actor
         "is_sender" : False,
@@ -114,6 +115,7 @@ nodes_config = {
         "protection_time" : 1000, # 1000 ms = 1 sec
         "beacon_interval" : 240, # 120 seconds
         "beacon_jitter" : 10,    # seconds, will vary the above value by +- 10 seconds
+        "gpio_button_irq" : 13, # IRQ on pin 13
     },
     "1054058" : { # Ultrasonic actor
         "is_sender" : False,
@@ -124,4 +126,14 @@ nodes_config = {
         "action_cancel_previous" : True,
         "action_duration"  : 10000, # 10000 ms = 10 sec
     },
+        "951712297" : { # Flash Actor
+            "is_sender" : False,
+            "receiver_type" : packets.BasePacket.TYPE_ACTOR_FLASH,
+            "beacon_interval" : 120, # 120 seconds
+            "beacon_jitter" : 10,    # seconds, will vary the above value by +- 10 seconds
+            "battery_type" : "analog", # Read battery from Pin 36
+            "action_cancel_previous" : True,
+            "action_duration"  : 10000, # 10000 ms = 10 sec
+            "gpio_led_status" : 13, # Status LED on pin 13
+        },
 }
